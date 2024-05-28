f60cb8af7da567604b9ec3b034b4d0ee36ba9188
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2024-05-28 23:55:50 +0600

    created emoji picker and display emoji on the screen

--- App.js
+++ App.js
@@ -7,12 +7,16 @@ import { useState } from "react";
 import IconButton from "./components/IconButton";
 import CircleButton from "./components/CircleButton";
 import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from "./components/EmojiList";
+import EmojiSticker from "./components/EmojiSticker";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+
   // imagepicker setting
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -37,9 +41,20 @@ export default function App() {
   const onSaveImageAsync = () => {
     // will implement this later
   };
-  const onModalClose = () => {};
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
+      </View>
       <Text style={{ color: "green" }}>
         Open up App.js to start working on your app!
       </Text>
@@ -76,7 +91,7 @@ export default function App() {
         </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        {/* A list of emoji component will go here */}
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
     </View>
--- components/EmojiPicker.js
+++ components/EmojiPicker.js
@@ -1,18 +1,20 @@
 import { Modal, View, Text, Pressable, StyleSheet } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-export default function EmojiPicker({ isVisible, children, onPress }) {
-  <Modal animationType="slide" transparent={true} visible={isVisible}>
-    <View style={styles.modalContent}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Choose a sticker</Text>
-        <Pressable onPress={onPress}>
-          <MaterialIcons name="close" color="#fff" size={22} />
-        </Pressable>
+export default function EmojiPicker({ isVisible, children, onClose }) {
+  return (
+    <Modal animationType="slide" transparent={true} visible={isVisible}>
+      <View style={styles.modalContent}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Choose a sticker</Text>
+          <Pressable onPress={onClose}>
+            <MaterialIcons name="close" color="#fff" size={22} />
+          </Pressable>
+        </View>
+        {children}
       </View>
-      {children}
-    </View>
-  </Modal>;
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
